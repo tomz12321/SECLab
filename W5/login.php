13882fd218c534331186d2beb5c2c82a7c258b6c
tomz123 <tomz12321@gmail.com>
2018-08-20 16:17:50 +1000

    Submission of Lab5
    Functionality completed:
    - UserLogin
    - UserRegister

---
+++ W5/login.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+ 
+<?php
+            	
+	//Receive input from clint side
+	$usrname = $_POST['usr'];
+	$password = $_POST['pwd'];
+            	
+    $login = 0;
+    //read users.txt line by line
+ 	foreach(file('users.txt') as $line) {
+		list($usr,$pwd) = explode(",",$line);
+    //verify if an exist user with the same username
+    if($usr == $usrname){
+    	//verify the password
+    	if($pwd == $password."\n"){
+			$login = 1;
+			break;
+                	}
+            }
+    }
+            	
+            	if($login==0){
+                            	echo "Wrong Username or Password!";
+            	}else{
+                            	echo "Login successful!";
+            	}
+            	
+            	echo "<br/>Go <a href='login.html'>back</a> to register, login or check the users.txt";
+ 
+?>
+ 
+ 
+</body>
+</html>
---
+++ W5/register.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+
+<?php
+	
+	//Receive input from clint side
+	$usrname = $_POST['usr'];
+	$password = $_POST['pwd'];
+	
+	//check if the input exist
+	$exist = 0;
+
+//read the file line by line
+
+	foreach(file('users.txt') as $line) {
+		list($usr,$pwd) = explode(",",$line);
+		//compare the content of the input and the line
+		if($usr == $usrname){
+			$exist = 1;
+			break;
+		}
+	}
+	
+	if($exist == 1){
+		echo "The input is exist! <br/><br/>Please enter another one via <a href='register.html'>register.html</a>";
+	}else{
+		//open a file named "users.txt"
+		$file = fopen("users.txt","a");
+		//insert this input (plus a newline) into the database.txt
+		fwrite($file,$usrname.",".$password."\n");
+		//close the "$file"
+		fclose($file);
+		echo "The input is added to the users.txt. <br/><br/>Please try to enter the same input again via <a href='register.html'>register.html</a>";
+	}
+?>
+
+</body>
+</html>
